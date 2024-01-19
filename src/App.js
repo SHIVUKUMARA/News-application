6618468900319259ba08d897a6ef78069ca7468e
SHIVUKUMARA <shivukumaraspatil01@gmail.com>
2024-01-19 14:45:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Newsboard from "./Components/Newsboard";
 
 function App() {
+
+  const [category, setCategory] = useState("general");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <Newsboard category={category} />
     </div>
   );
 }
